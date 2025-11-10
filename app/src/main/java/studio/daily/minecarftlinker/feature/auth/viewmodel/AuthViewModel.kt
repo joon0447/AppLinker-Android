df3f180762cacedf02ff6614c9c57e04f3d0cd82
joon0447 <joon0447@naver.com>
2025-11-10 21:51:25 +0900

    feat(viewmodel) : 파이어베이스 연결 함수 추가

--- app/src/main/java/studio/daily/minecarftlinker/feature/auth/viewmodel/AuthViewModel.kt
+++ app/src/main/java/studio/daily/minecarftlinker/feature/auth/viewmodel/AuthViewModel.kt
@@ -1,11 +1,20 @@
 package studio.daily.minecarftlinker.feature.auth.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class AuthViewModel : ViewModel() {
 
+    private val db = FirebaseFirestore.getInstance()
+
+    private val _uuid = MutableStateFlow<String?>(null)
+    val uuid: StateFlow<String?> = _uuid
     private val _authCode = MutableStateFlow("")
     val authCode: StateFlow<String> = _authCode
 
@@ -17,5 +26,27 @@ class AuthViewModel : ViewModel() {
         _isCodeValid.value = code.length == 6
     }
 
-    fun connectToServer() {}
+    fun connectToServer() {
+        val code = _authCode.value
+
+        viewModelScope.launch {
+            try{
+                val snapshot = db.collection("linkCodes")
+                    .whereEqualTo("code", code)
+                    .limit(1)
+                    .get()
+                    .await()
+
+                if(!snapshot.isEmpty) {
+                    val document = snapshot.documents.first()
+
+                    val uuid = document.id
+                    _uuid.value = uuid
+                    Log.d("AuthViewModel", "uuid : $uuid")
+                }
+            } catch (e: Exception) {
+                Log.e("AuthViewModel", "오류", e)
+            }
+        }
+    }
 }
