5a3cd1c23b8b80d680eaeaa61d35137a27b9f17a
joon0447 <joon0447@naver.com>
2025-11-11 23:26:40 +0900

    feat(network): mojang API 호출 추가

---
+++ app/src/main/java/studio/daily/minecraftlinker/core/network/MojangApi.kt
@@ -0,0 +1,46 @@
+package studio.daily.minecraftlinker.core.network
+
+import androidx.compose.ui.text.style.Hyphens
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MojangApi {
+    @GET("session/minecraft/profile/{uuid}")
+    suspend fun getProfile(
+        @Path("uuid") uuidWithoutHyphens: String
+    ): MojangProfileResponse
+}
+
+data class MojangProfileResponse(
+    val id: String,
+    val name: String,
+    val properties: List<MojangProperty>
+)
+
+data class MojangProperty(
+    val name: String,
+    val value: String,
+    val signature: String? = null
+)
+
+data class TexturesPayload(
+    val timestamp: Long? = null,
+    val profileId: String? = null,
+    val profileName: String? = null,
+    val textures: TexturesMap? = null
+)
+
+data class TexturesMap(
+    @SerializedName("SKIN") val skin: SkinInfo? = null,
+    @SerializedName("CAPE") val cape: CapeInfo? = null
+)
+
+data class SkinInfo(
+    val url: String,
+    val metadata: Map<String, Any>? = null
+)
+
+data class CapeInfo(
+    val url: String
+)
---
+++ app/src/main/java/studio/daily/minecraftlinker/core/network/RetrofitProvider.kt
@@ -0,0 +1,40 @@
+package studio.daily.minecraftlinker.core.network
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitProvider {
+    private val gson = GsonBuilder()
+        .disableHtmlEscaping()
+        .create()
+
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                }
+            )
+            .build()
+    }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://sessionserver.mojang.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+    }
+
+    val mojangApi: MojangApi by lazy {
+        retrofit.create(MojangApi::class.java)
+    }
+
+}
