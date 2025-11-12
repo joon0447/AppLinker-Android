5b289c965106fb863bd9e31c459b10852105f3ab
joon0447 <joon0447@naver.com>
2025-11-12 21:45:00 +0900

    feat(viewmodel): 서버 API 뷰모델 생성

---
+++ app/src/main/java/studio/daily/minecraftlinker/feature/home/login/viewmodel/ServerViewModel.kt
@@ -0,0 +1,28 @@
+package studio.daily.minecraftlinker.feature.home.login.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import studio.daily.minecraftlinker.core.network.server.ServerResponse
+import studio.daily.minecraftlinker.feature.home.login.repository.ServerRepository
+
+class ServerViewModel: ViewModel() {
+
+    private val repository = ServerRepository()
+
+    private val _serverResponse = MutableStateFlow<ServerResponse?>(null)
+    val serverResponse: StateFlow<ServerResponse?> = _serverResponse
+
+    fun loadPlayers() {
+        viewModelScope.launch {
+            try{
+                val response = repository.fetchPlayers()
+                _serverResponse.value = response
+            } catch (e: Exception) {
+                println("API 에러 : ${e.message}")
+            }
+        }
+    }
+}
