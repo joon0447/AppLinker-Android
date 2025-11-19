228885ae4a52da9475380fdced36911578cf16b2
joon0447 <joon0447@naver.com>
2025-11-19 11:05:49 +0900

    refactor(theme) : 상태바 아이콘 색상 변경

--- app/src/main/java/studio/daily/minecraftlinker/ui/theme/Theme.kt
+++ app/src/main/java/studio/daily/minecraftlinker/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package studio.daily.minecraftlinker.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,9 +55,31 @@ fun MinecarftLinkerTheme(
         else -> LightColorScheme
     }
 
+    SetStatusBar(
+        backgroundColor = colorScheme.background,
+        darkIcons = true
+    )
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+fun SetStatusBar(
+    backgroundColor: Color,
+    darkIcons: Boolean
+) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = backgroundColor.toArgb()
+
+            val controller = WindowCompat.getInsetsController(window, view)
+            controller.isAppearanceLightStatusBars = darkIcons
+        }
+    }
 }
